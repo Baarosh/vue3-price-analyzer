41b404b3df10a4cd763b59145f333466d2555177
Baarosh <45597075+Baarosh@users.noreply.github.com>
2021-11-25 18:35:07 +0100

    Added server.js

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>Hello</div>
+</template>
+
+<script>
+export default {
+name: 'App',
+component: {},
+props: {},
+emits: {},
+data() { return {}},
+computed: {},
+methods: {},
+watch: {},
+beforeCreated() {},
+created() {},
+beforeMounted() {},
+mounted() {},
+}
+</script>

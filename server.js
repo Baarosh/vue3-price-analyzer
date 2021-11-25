7a1589b71b72bcc9f5fc02fefc80e5ffb85f36fc
Baarosh <45597075+Baarosh@users.noreply.github.com>
2021-11-25 18:55:05 +0100

    test

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (request, response) => {
-    response.send('Hello World from server!')
+    response.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(port, () => {
-    console.log('Server is listening...')
+    console.log(`Server is listening...PORT: ${port}`)
 })
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-<div>Hello</div>
+<div>Hello World</div>
+aaaaaaaaaa
 </template>
 
 <script>

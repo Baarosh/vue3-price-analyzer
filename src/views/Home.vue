e2e475054937f7ae3b29df9e51492d095740ceae
Baarosh <45597075+Baarosh@users.noreply.github.com>
2021-11-26 23:20:24 +0100

    Connected to Firebase

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
-    <router-view></router-view>
+    <div>
+        <h1>HOME VIEW</h1>
+    </div>
 </template>
 
 <script>
 export default {
-name: 'App',
+name: 'Home',
 component: {},
 props: {},
 emits: {},

bae7f6d15224e242869ad04da791666f2e1295fa
Baarosh <45597075+Baarosh@users.noreply.github.com>
2021-12-31 11:32:21 +0100

    Update ms.config.js

--- server/utilities/mysql/ms.config.js
+++ server/utilities/mysql/ms.config.js
@@ -1,11 +1,11 @@
 import ck from 'ckey'
 
 const mySQLConfig = {
-  host: ck.SQL_HOSTG,
-  user: ck.SQL_USERG,
-  password: ck.SQL_PASSWORDG,
-  database: ck.SQL_DATABASEG
-  // port: ck.SQL_PORT
+  host: process.env.SQL_HOSTG,
+  user: process.env.SQL_USERG,
+  password: process.env.SQL_PASSWORDG,
+  database: process.env.SQL_DATABASEG,
+  port: process.env.SQL_PORTG
   };
 
   export default mySQLConfig
